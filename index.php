f63ff705176729f277f496e328e06b998cb3bb46
chysharmila <sharmilchudh547@gmail.com>
2019-01-25 14:23:21 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="assets/css/index1.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+  <script src="assets/js/jquery.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid ">
+  <div class="navbar navbar-fixed-top">
+    <div class="navbar-header">
+      <a href="#" class="navbar-brand">Khaza.com</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Products</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+  </div>
+
+
+
+	<img src="plaza/images114.jpg" class="img-responsive" width="100%" style="height: 285px;">
+	<h1 class="top"><b>Contact us</b></h1>
+  <br><br><br>
+  <div class="row">
+  	<div class="col-sm-2 color div1" >
+  		<h3 class="center" ><b>Address</b></h3>
+  		<p class="center">Dhangadhi,Kailali,Nepal</p>
+  	</div>
+  	<div class="col-sm-2 color div2" >
+  		<h3  class="center"><b>Phone</b></h3>
+  		<p class="center">9898766775,<br>9995656567,<br>9848454567</p>
+  	</div>
+  	<div class="col-sm-2  color div3" >
+  		<h3 class="center" ><b>Email</b></h3>
+  		<p class="center">info@onlinekhaja.com</p>
+  	</div>
+
+  </div>
+
+  <br><br><br><br><br><br>
+  <h2 class="center">OUR LOCATION</h2>
+  <br><br><br>
+  <div  class="row-fluid">
+         <div class="span8">
+               <iframe width="1319px" height="200px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=15+Springfield+Way,+Hythe,+CT21+5SH&aq=t&sll=52.8382,-2.327815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed"></iframe>
+               </div>
+             </div>
+             <br><br><br><br><br><br><br><br>
+      
+  <h2 class="center">Drop A Line</h2>
+  <h5 class="center">Get in Touch</h5>
+  <form style="margin-left: 354px;
+margin-right: 312px;">
+    <div class="form-group col-sm-6">
+    <input type="text" name="name" placeholder="Your Name" class="form-control border " style="border-radius: 27px;">
+    <input type="Email" name="Email" placeholder="Your Email" class="form-control border" style="margin-top: -33px;margin-left: 323px; border-radius: 27px;">
+  </div>
+  <div class="form-group">
+    <textarea name="message" class="form-control border" id="message" placeholder="Your Message" rows="10" cols="30" style="height: 236px; border-radius: 27px;"></textarea>
+
+
+  </div>
+  <button type="submit" class="btn btn-primary"  style="background-color: green;font-size:large;">SUBMIT</button>
+  </form>
+  </div>
+ 
+<br><br><br>
+   <footer class="page-footer font-small blue pt-4 " >
+                        <div class="row">
+                            
+                            <div class="col-sm-3 ">
+                              <h4>ABOUT STORE</h4>
+                              <p class="hi">Facilis ipsum reprehenderit nemo<br>molestias.Aut cum mollitia reprehenderit<br>Eos cumque dicta adipisci architecto culpa<br>amet.</p>
+                            </div>
+                            <div class="col-md-2">
+                                <h4>CUSTOMER CARE</h4><br>
+                                 <ul class="list-unstyled ">
+                                <li><a class="hi" href="#">Contact</a></li><br>
+                                <li><a class="hi"href="#">Return/Exchange</a></li><br>
+                                <li><a class="hi" href="#">Gift Voucher</a></li><br>
+                                <li><a class="hi"href="#">Wishlist</a></li><br>
+                                <li><a class="hi" href="#">Special</a></li><br>
+                                <li><a class="hi"href="#">Customer Services</a></li><br>
+                                <li><a class="hi"href="#">site maps</a></li><br>
+                                </ul>
+                                    </div>
+                            <div class="col-md-2">
+                              <h4>INFORMATION</h4><br>
+                                 <ul class="list-unstyled ">
+                                
+                                <li><a class="hi" href="#">Abuot us</a></li><br>
+                                <li><a class="hi"href="#">Delivery information</a></li><br>
+                                <li><a class="hi" href="#">Privacy Policy</a></li><br>
+                                <li><a class="hi" href="#">Support</a></li><br>
+                                <li><a class="hi" href="#">Order Tracking</a></li><br>
+                                </ul>
+                            </div>
+                            <div class="col-sm-2">
+                               
+                             <h4>NEWS</h4><br>
+                                <ul class="list-unstyled ">
+                                    <li><a class="hi"href="#">Blog</a></li><br>
+                                    <li><a class="hi"href="#"> Press</a></li><br>
+                                    <li><a class="hi"href="#">Exhibitions</a></li><br>
+                                   
+                            </div>
+                            <div class="col-sm-3 ">
+                              <h4>CONTACT INFORMATION</h4>
+                              <p class="hi">291 South 21th street,<br>Sute 721 New York NY 10016<br>+1235 2355 98<br>info@yoursite.com<br>Yoursite.com</p>
+                            </div>
+                        </div>
+                       
+                    </footer>
+</div>
+</body>
+</html>
